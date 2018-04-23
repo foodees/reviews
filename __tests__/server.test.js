5a4e2403e47a68b46cdd671af4f15f5ba0b64f23
Raymond Cabrera <rjgcengr@gmail.com>
2018-04-23 16:21:18 -0700

    Add tests for /reviews/ratingdesc

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -10,7 +10,11 @@ let userData = require('../dummyData/userData.js');
 let reviewAscData = require('../dummyData/reviewAscData.js');
 let userAscData = require('../dummyData/userAscData.js');
 
+//dummy data for /reviews/ratingdesc
+let reviewRatingData = require('../dummyData/reviewRatingData.js');
+let userRatingData = require('../dummyData/userRatingData.js');
 
+//dummy data for /reviews/asc
 
 describe('Test the all endpoints for correct responses with supertest', () => {
     test('/reviews/restaurant should return response with correct restaurant name', (done) => {
@@ -20,6 +24,7 @@ describe('Test the all endpoints for correct responses with supertest', () => {
         done();
       });
     });
+
 
     test('/reviews/desc should return response with correct reviews, 10 at a time', (done) => {
       request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/desc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=1').then(response => {
@@ -41,6 +46,7 @@ describe('Test the all endpoints for correct responses with supertest', () => {
       });
     });
 
+
     test('/reviews/asc should return response with correct reviews, 10 at a time', (done) => {
       request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/asc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=1').then(response => {
         expect(response.statusCode).toBe(200);
@@ -61,15 +67,26 @@ describe('Test the all endpoints for correct responses with supertest', () => {
       });
     });
 
-    // test('/reviews/ratingdesc should return response with correct reviews, 10 at a time', (done) => {
-    //   request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/desc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=10').then(response => {
-    //     expect(response.statusCode).toBe(200);
-    //     expect(response.body.reviews.length).toBe(10);
-    //     expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewData0));
-    //     expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userDataPage10));
-    //     done();
-    //   });
-    // });
+
+    test('/reviews/ratingdesc should return response with correct reviews, 10 at a time', (done) => {
+      request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingdesc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=1').then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.reviews.length).toBe(10);
+        expect(response.body.users.length).toBe(10);
+        expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingData.pageOne));
+        expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingData.pageOne));
+        done();
+      });
+
+      request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingdesc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=10').then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.reviews.length).toBe(10);
+        expect(response.body.users.length).toBe(10);
+        expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingData.pageTen));
+        expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingData.pageTen));
+        done();
+      });
+    });
 
     // test('/reviews/ratingasc should return response with correct reviews, 10 at a time', (done) => {
     //   // request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingdesc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=1').then(response => {
