4371e6582043d44fafd4f2bd3e32d890ebbd6764
Raymond Cabrera <rjgcengr@gmail.com>
2018-04-23 19:33:18 -0700

    Add tests for /reviews/search/desc endpoint

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -38,6 +38,12 @@ jest.mock('userSearchData');
 let reviewSearchAscData = require('reviewSearchAscData');
 let userSearchAscData = require('userSearchAscData');
 
+//dummy data for /reviews/search/ratingdesc
+jest.mock('reviewRatingSearchData');
+jest.mock('userRatingSearchData');
+let reviewRatingSearchData = require('reviewRatingSearchData');
+let userRatingSearchData = require('userRatingSearchData');
+
 
 describe('Test the all endpoints for correct responses with supertest', () => {
     test('/reviews/restaurant should return response with correct restaurant name', (done) => {
@@ -174,26 +180,45 @@ describe('Test the all endpoints for correct responses with supertest', () => {
     });
 
 
-    // test('/reviews/ratingdesc should return response with correct reviews, 10 at a time', (done) => {
-    //   request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingdesc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=1').then(response => {
+    test('/reviews/search/ratingdesc should return response with correct reviews, 10 at a time', (done) => {
+      request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/search/ratingdesc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=1&term=steak').then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.reviews.length).toBe(10);
+        expect(response.body.users.length).toBe(10);
+        expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingSearchData.pageOne));
+        expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingSearchData.pageOne));
+        done();
+      });
+
+      request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/search/ratingdesc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=10&term=steak').then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.reviews.length).toBe(1);
+        expect(response.body.users.length).toBe(1);
+        expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingSearchData.pageTen));
+        expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingSearchData.pageTen));
+        done();
+      });
+    });
+
+    // test('/reviews/ratingasc should return response with correct reviews, 10 at a time', (done) => {
+    //   request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingasc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=1').then(response => {
     //     expect(response.statusCode).toBe(200);
     //     expect(response.body.reviews.length).toBe(10);
     //     expect(response.body.users.length).toBe(10);
-    //     expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingData.pageOne));
-    //     expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingData.pageOne));
+    //     expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingAscData.pageOne));
+    //     expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingAscData.pageOne));
     //     done();
     //   });
     //
-    //   request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingdesc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=10').then(response => {
+    //   request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingasc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=10').then(response => {
     //     expect(response.statusCode).toBe(200);
     //     expect(response.body.reviews.length).toBe(10);
     //     expect(response.body.users.length).toBe(10);
-    //     expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingData.pageTen));
-    //     expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingData.pageTen));
+    //     expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingAscData.pageTen));
+    //     expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingAscData.pageTen));
     //     done();
     //   });
     // });
-
 
     test('/reviews/getCount should return response the number of reviews for a restaurant', (done) => {
       request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/count/?id=--9e1ONYQuAa-CB_Rrw7Tw').then(response => {
