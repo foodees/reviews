c63c949b388f39897323c0170b1d7b557cd6d4c0
Raymond Cabrera <rjgcengr@gmail.com>
2018-04-23 17:34:04 -0700

    Add tests for /reviews/ratingasc endpoint

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -21,6 +21,11 @@ let reviewRatingData = require('reviewRatingData');
 let userRatingData = require('userRatingData');
 
 //dummy data for /reviews/asc
+jest.mock('reviewRatingAscData');
+jest.mock('userRatingAscData');
+let reviewRatingAscData = require('reviewRatingAscData');
+let userRatingAscData = require('userRatingAscData');
+
 
 describe('Test the all endpoints for correct responses with supertest', () => {
     test('/reviews/restaurant should return response with correct restaurant name', (done) => {
@@ -94,15 +99,26 @@ describe('Test the all endpoints for correct responses with supertest', () => {
       });
     });
 
-    // test('/reviews/ratingasc should return response with correct reviews, 10 at a time', (done) => {
-    //   // request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingdesc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=1').then(response => {
-    //   //   expect(response.statusCode).toBe(200);
-    //   //   expect(response.body.reviews.length).toBe(10);
-    //   //   expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewDataPage10));
-    //   //   expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userDataPage10));
-    //   //   done();
-    //   // });
-    // });
+
+    test('/reviews/ratingasc should return response with correct reviews, 10 at a time', (done) => {
+      request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingasc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=1').then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.reviews.length).toBe(10);
+        expect(response.body.users.length).toBe(10);
+        expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingAscData.pageOne));
+        expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingAscData.pageOne));
+        done();
+      });
+
+      request(server.app).get('/biz/--9e1ONYQuAa-CB_Rrw7Tw/reviews/ratingasc/?id=--9e1ONYQuAa-CB_Rrw7Tw&page=10').then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.reviews.length).toBe(10);
+        expect(response.body.users.length).toBe(10);
+        expect(JSON.stringify(response.body.reviews)).toBe(JSON.stringify(reviewRatingAscData.pageTen));
+        expect(JSON.stringify(response.body.users)).toBe(JSON.stringify(userRatingAscData.pageTen));
+        done();
+      });
+    });
 
     // ...
 
